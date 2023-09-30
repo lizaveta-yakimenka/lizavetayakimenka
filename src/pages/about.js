7267b7482252fc7724541ea2ec752ff4bf1c67c5
lizaveta-yakimenka <z1977579@students.niu.edu>
2023-09-30 17:00:44 -0500

    about

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,16 @@
-import * as React from "react"
+// Step 1: Import React
+import * as React from 'react'
 
-export default function Component () {
-  return "Hello world"
+// Step 2: Define your component
+const AboutPage = () => {
+  return (
+    <main>
+      <h1>About Me</h1>
+      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+    </main>
+  )
 }
+
+export const Head = () => <title>About Me</title>
+// Step 3: Export your component
+export default AboutPage
